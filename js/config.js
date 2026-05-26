5ea0a0e99ed51a0f39b08bd5c07750d55e60a81d
HisaabMitra <salinder.arya01@gmail.com>
2026-05-26 22:25:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,7 @@
+// js/config.js
 
-// Blank configuration file for future use
+const CONFIG = {
+    // Replace these strings with your actual Supabase project credentials
+    SUPABASE_URL: "https://nwfiaypdwtyetobchsxk.supabase.co",
+    SUPABASE_KEY: "sb_publishable_CZ_0s-JItMjSoRSRJXDbDg_BTD1mUCf"
+};
