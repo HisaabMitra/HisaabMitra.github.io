9ff91cafb9b9fb1af56edbe9e38dff0ff461fc29
HisaabMitra <salinder.arya01@gmail.com>
2026-06-03 18:01:30 +0530

    Update supabase.js

--- js/supabase/supabase.js
+++ js/supabase/supabase.js
@@ -6,5 +6,3 @@ if (!window.supabase) {
 
 // Attach the client directly to the global window scope
 window.supabaseClient = window.supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
-
-console.log("Supabase Client initialized successfully.");
